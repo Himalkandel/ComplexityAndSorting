d43606675063b96b022d2255443447617aa84363
kevinh63 <kevin.h.huang@edu.sait.ca>
2023-10-17 15:46:43 -0600

    fixed selection sort
    bug that was causing selection sort to crash was fixed

--- Assignment1/src/Utils/SortingUtility.java
+++ Assignment1/src/Utils/SortingUtility.java
@@ -81,16 +81,19 @@ public class SortingUtility {
 
     public static void selectionSort(GeometricShape[] arr) {
         int n = arr.length;
-        for (int i = 0; 1 < n - 1; i++) {
+        for (int i = 0; i < n - 1; i++) {
             int minIndex = i;
-            for (int j = i + 1; j < n; j++)
-                if (arr[j].compareTo(arr[minIndex]) < 0)
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j].compareTo(arr[minIndex]) < 0) {
                     minIndex = j;
+                }
+            }
             GeometricShape temp = arr[minIndex];
             arr[minIndex] = arr[i];
             arr[i] = temp;
         }
     }
+    
 
     public static void mergeSort(GeometricShape[] arr) {
         mergeSort(arr, 0, arr.length - 1);
