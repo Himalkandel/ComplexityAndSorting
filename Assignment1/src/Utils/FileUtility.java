184ef8a7645eea00c110f22353c7f82203bf5aea
himalsait <120237214+himalsait@users.noreply.github.com>
2023-10-17 10:46:02 -0600

    update: method
    the sorting method works the current error is like i said on group chat, unknown shape. to run u have to run it as an app (go to debug tab on side panel (the bug with a run button) and change it to app)

--- Assignment1/src/Utils/FileUtility.java
+++ Assignment1/src/Utils/FileUtility.java
@@ -15,43 +15,43 @@ public class FileUtility {
 
     public static ArrayList<GeometricShape> readShapesFromFile(String filePath) throws Exception {
         ArrayList<GeometricShape> shapeList = new ArrayList<>();
-
+    
         // Reading the shapes from file
         File file = new File(filePath);
         Scanner scanner = new Scanner(file);
-
-        while(scanner.hasNext()) {
-            String line = scanner.nextLine();
-            String[] parts = line.split(" ");
-            String shapeType = parts[0];
-
-            switch(shapeType) {
+        String line = scanner.nextLine();
+        String[] parts = line.split(" ");
+    
+        for (int i = 0; i < parts.length; i+=3) { // We're increasing by 3 since there's a name followed by 2 parameters for each shape
+            String shapeType = parts[i];
+    
+            switch (shapeType) {
                 case "Cylinder":
-                    Cylinder cylinder = new Cylinder(Double.parseDouble(parts[1]), Double.parseDouble(parts[2]));
+                    Cylinder cylinder = new Cylinder(Double.parseDouble(parts[i+1]), Double.parseDouble(parts[i+2]));
                     shapeList.add(cylinder);
                     break;
                 case "Pyramid":
-                    Pyramid pyramid = new Pyramid(Double.parseDouble(parts[1]), Double.parseDouble(parts[2]));
+                    Pyramid pyramid = new Pyramid(Double.parseDouble(parts[i+1]), Double.parseDouble(parts[i+2]));
                     shapeList.add(pyramid);
                     break;
                 case "OctagonalPrism":
-                    OctagonalPrism octagonalPrism = new OctagonalPrism(Double.parseDouble(parts[1]), Double.parseDouble(parts[2]));
+                    OctagonalPrism octagonalPrism = new OctagonalPrism(Double.parseDouble(parts[i+1]), Double.parseDouble(parts[i+2]));
                     shapeList.add(octagonalPrism);
                     break;
                 case "PentagonalPrism":
-                    PentagonalPrism pentagonalPrism = new PentagonalPrism(Double.parseDouble(parts[1]), Double.parseDouble(parts[2]));
+                    PentagonalPrism pentagonalPrism = new PentagonalPrism(Double.parseDouble(parts[i+1]), Double.parseDouble(parts[i+2]));
                     shapeList.add(pentagonalPrism);
                     break;
                 case "SquarePrism":
-                    SquarePrism squarePrism = new SquarePrism(Double.parseDouble(parts[1]), Double.parseDouble(parts[2]));
+                    SquarePrism squarePrism = new SquarePrism(Double.parseDouble(parts[i+1]), Double.parseDouble(parts[i+2]));
                     shapeList.add(squarePrism);
                     break;
                 case "TriangularPrism":
-                    TriangularPrism triangularPrism = new TriangularPrism(Double.parseDouble(parts[1]), Double.parseDouble(parts[2]));
+                    TriangularPrism triangularPrism = new TriangularPrism(Double.parseDouble(parts[i+1]), Double.parseDouble(parts[i+2]));
                     shapeList.add(triangularPrism);
                     break;
                 case "Cone":
-                    Cone cone = new Cone(Double.parseDouble(parts[1]), Double.parseDouble(parts[2]));
+                    Cone cone = new Cone(Double.parseDouble(parts[i+1]), Double.parseDouble(parts[i+2]));
                     shapeList.add(cone);
                     break;
                 default:
@@ -59,8 +59,9 @@ public class FileUtility {
                     break;
             }
         }
-
+    
         scanner.close();
         return shapeList;
     }
+    
 }
