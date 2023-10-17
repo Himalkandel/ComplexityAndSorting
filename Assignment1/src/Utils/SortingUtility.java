316a8ceff92c3ac2db519811626ccbdc81ca7010
himalsait <120237214+himalsait@users.noreply.github.com>
2023-10-17 12:18:13 -0600

    insertion sort added
    added the insertionsort into the sortingutility

--- Assignment1/src/Utils/SortingUtility.java
+++ Assignment1/src/Utils/SortingUtility.java
@@ -16,4 +16,20 @@ public class SortingUtility {
                     arr[j+1] = temp;
                 }
     }
+
+    public static void insertionSort(GeometricShape[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            GeometricShape key = arr[i];
+            int j = i - 1;
+
+            // Move elements of arr[0..i-1] that are greater than key
+            // to one position ahead of their current position
+            while (j >= 0 && arr[j].compareTo(key) > 0) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
 }
