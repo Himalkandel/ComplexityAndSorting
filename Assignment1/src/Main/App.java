b9f77807f31f4e31a6916bcbf11f0b4941872fbc
himalsait <120237214+himalsait@users.noreply.github.com>
2023-10-17 12:31:29 -0600

    app.java
    updated app.java for user inputs
    it aint working but it a good baseline for future uses

--- Assignment1/src/Main/App.java
+++ Assignment1/src/Main/App.java
@@ -1,23 +1,60 @@
 package Main;
 
-import Shapes.*;
-import Utils.*;
+import Utils.SortingUtility;
+import Utils.FileUtility;
+import Shapes.GeometricShape;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Scanner;
+
+
+//mainly to get user input on which selection to use
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // Assuming file path is the first argument. Adjust accordingly.
-        ArrayList<GeometricShape> shapes = FileUtility.readShapesFromFile(args[0]);
+        Scanner scanner = new Scanner(System.in);
 
-        // Sorting based on height
-        Collections.sort(shapes);
+        try {
+            System.out.println("Starting the application...");
 
-        // Sorting based on base area
-        Collections.sort(shapes, new BaseAreaComparator());
+            if (args.length == 0) {
+                System.out.println("No file path provided!");
+                return;
+            }
 
-        // Sorting based on volume
-        Collections.sort(shapes, new VolumeComparator());
+            // this reads using fileutility
+            System.out.println("Reading shapes from file...");
+            ArrayList<GeometricShape> shapes = FileUtility.readShapesFromFile(args[0]);
 
+            System.out.println("Choose a sorting method:");
+            System.out.println("1. Bubble Sort");
+            System.out.println("2. Insertion Sort");
+            // add the options based on case numbers
+
+            scanner.nextLine();  // leftover newlines
+            System.out.println("Waiting for user input...");
+            int choice = scanner.nextInt();
+
+            GeometricShape[] shapesArray = shapes.toArray(new GeometricShape[0]);
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Performing Bubble Sort...");
+                    SortingUtility.bubbleSort(shapesArray);
+                    System.out.println("Sorted using Bubble Sort!");
+                    break;
+                case 2:
+                    System.out.println("Performing Insertion Sort...");
+                    SortingUtility.insertionSort(shapesArray);
+                    System.out.println("Sorted using Insertion Sort!");
+                    break;
+                // Add the other sorting method cases here
+                default:
+                    System.out.println("Invalid choice!");
+            }
+
+            System.out.println("Application finished.");
+        } finally {
+            scanner.close(); // Ensure the scanner object is always closed
+        }
     }
 }
