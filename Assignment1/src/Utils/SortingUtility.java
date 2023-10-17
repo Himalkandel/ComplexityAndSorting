b4dc530b283b5debd2890bdca9ec3a7e7c2a13c2
Liam Marrie <liam.marrie@edu.sait.ca>
2023-10-17 13:43:48 -0600

    heapsort push

--- Assignment1/src/Utils/SortingUtility.java
+++ Assignment1/src/Utils/SortingUtility.java
@@ -7,13 +7,13 @@ public class SortingUtility {
 
     public static void bubbleSort(GeometricShape[] arr) {
         int n = arr.length;
-        for (int i = 0; i < n-1; i++)
-            for (int j = 0; j < n-i-1; j++)
-                if (arr[j].compareTo(arr[j+1]) > 0) {
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j].compareTo(arr[j + 1]) > 0) {
                     // swap arr[j+1] and arr[j]
                     GeometricShape temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
     }
 
@@ -31,5 +31,52 @@ public class SortingUtility {
             }
             arr[j + 1] = key;
         }
+    }
+
+    // Heap sort
+    public static void heapSort(GeometricShape[] arr) {
+        int n = arr.length;
+
+        // build max heap size
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        // extract elements one by one from the heap
+        for (int i = n - 1; i > 0; i--) {
+            // move max element to the end of the heap
+            GeometricShape temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // call max heapify on reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+
+    private static void heapify(GeometricShape[] arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 1;
+
+        // if left element is larger than the root
+        if (left < n && arr[left].compareTo(arr[largest]) > 0) {
+            largest = left;
+        }
+
+        // if right element is larger than the largest so far
+        if (right < n && arr[right].compareTo(arr[largest]) > 0) {
+            largest = right;
+        }
+
+        // if largest element is not root
+        if (largest != i) {
+            GeometricShape swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            heapify(arr, n, largest);
+        }
+
     }
 }
